3fdfca787136479ef16464eb03f199afd28fa50e
Seth <seth@seth>
2018-11-27 00:31:29 +1300

    Finished Auth Flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+if (process.env.NODE_ENV = "production") {
+   //we are in a production environment, return the production keys
+   module.exports = require('./prod');
+} else {
+    // we are in a development environment, return the development keys
+    module.exports = require('./dev');
+
+}
